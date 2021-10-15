fad7e3298b136e32a81b31a84b210df6eb18eea2
Ramon Jansen <r.b.jansen03@gmail.com>
2021-10-15 09:46:12 +0200

    terminal pushed

---
+++ src/Entity/CsvFile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CsvFileRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=CsvFileRepository::class)
+ */
+class CsvFile
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
